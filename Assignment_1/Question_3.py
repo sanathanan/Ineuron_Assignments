49d1b0777034c98b9a577f6f0230630e5d049168
V.T. Sanathanan <75120579+sanathanan@users.noreply.github.com>
2021-02-02 21:04:41 +0530

    Add files via upload

---
+++ Assignment_1/Question_3.py
@@ -0,0 +1,15 @@
+# Assignment_1
+
+# Question: 3
+
+# Write a Python program to find the volume of sphere with diameter 12cm.
+# Formula : 4/3 * pi * r^3
+
+
+diameter = 12
+radius = diameter/2
+pi = 3.14
+
+volume_of_Sphere = (4/3)*(pi)*(radius**3)
+
+print(volume_of_Sphere)
